48d633271d41e4a9861ab473e77414e079dd51c3
merRen22 <merRen22@users.noreply.github.com>
2024-05-07 16:57:29 +0000

    deploy: 8682dcf29807cef9deb6459bd9fd0de90004f364

---
+++ _astro/hoisted.CyZvvZRo.js
@@ -0,0 +1,1 @@
+import"./hoisted.EAtev3D6.js";const e=document.getElementById("clotheSpecs"),t=document.getElementById("fixedTitleAction");function o(){e.scrollIntoView({block:"end",behavior:"smooth"})}t.addEventListener("click",o);
