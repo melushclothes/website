7f28212349e3fcaa3ed8964c0619c6feba662844
Renato <renatomercadoluna@hotmail.com>
2024-12-11 11:42:24 -0500

    update in legal and contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import svelte from '@astrojs/svelte';
-
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://melushstore.com",
   output: "server",
-  integrations: [tailwind(), icon(), svelte()],
+  integrations: [tailwind(), icon(), svelte(), sitemap()],
   adapter: cloudflare()
 });
